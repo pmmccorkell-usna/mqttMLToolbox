d653ff38538cb3636c0eaee32346fd71010f89b7
memberbs <35158605+memberbs@users.noreply.github.com>
2021-01-12 12:01:24 -0500

    adsf

--- installmqttMLToolbox.m
+++ installmqttMLToolbox.m
@@ -197,8 +197,51 @@ catch ME
     fprintf(2,'ERROR MESSAGE:\n\t%s\n',ME.message);
 end
 
+%% Check for successful download
+alternativeInstallMsg = [...
+    sprintf('Manually download the %s Toolbox using the following link:\n',toolboxName),...
+    newline,...
+    sprintf('%s\n',url),...
+    newline,...
+    sprintf('Once the file is downloaded:\n'),...
+    sprintf('\t(1) Unzip your download of the "%sToolbox"\n',toolboxName),...
+    sprintf('\t(2) Change your "working directory" to the location of "install%sToolbox.m"\n',toolboxName),...
+    sprintf('\t(3) Enter "install%sToolbox" (without quotes) into the command window\n',toolboxName),...
+    sprintf('\t(4) Press Enter.')];
 
+if ~confirm
+    warning('InstallToolbox:FailedDownload','Failed to download updated version of %s Toolbox.',toolboxName);
+    fprintf(2,'\n%s\n',alternativeInstallMsg);
+	
+    msgbox(alternativeInstallMsg, sprintf('Failed to download %s Toolbox',toolboxName),'warn');
+    return
+end
 
+%% Find base directory
+install_pos = strfind(fnames, sprintf('install%sToolbox.m',toolboxName) );
+sIdx = cell2mat( install_pos );
+cIdx = ~cell2mat( cellfun(@isempty,install_pos,'UniformOutput',0) );
+
+pname_star = fnames{cIdx}(1:sIdx-1);
+
+%% Get current directory and temporarily change path
+cpath = cd;
+cd(pname_star);
+
+%% Install Toolbox
+installToolbox(true);
+
+%% Move back to current directory and remove temp file
+cd(cpath);
+[ok,msg] = rmdir(pname,'s');
+if ~ok
+    warning('Unable to remove temporary download folder. %s',msg);
+end
+
+%% Complete installation
+fprintf('Installation complete.\n');
+
+end
 
 
 
